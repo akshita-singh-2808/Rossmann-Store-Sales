1ebbd2519d1363f4790262e7362c5439a6f36a13
Akshita Singh <akshita.singh916@gmail.com>
2026-06-08 06:14:57 +0530

    path change

--- dashboard/app.py
+++ dashboard/app.py
@@ -10,6 +10,8 @@ import joblib
 import warnings
 warnings.filterwarnings("ignore")
 import tensorflow 
+from pathlib import Path
+BASE_DIR = Path(__file__).parent
 
 plt.rcParams.update({
     "figure.facecolor": "#0E1117",
@@ -39,8 +41,9 @@ st.set_page_config(
 # ----------------------------------------------------------------
 @st.cache_data
 def load_data():
-    train = pd.read_csv("data\\rossmann-store-sales\\train.csv", low_memory=False)
-    store = pd.read_csv("data\\rossmann-store-sales\\store.csv")
+    train = pd.read_csv(BASE_DIR / "train.csv", low_memory=False)
+
+    store = pd.read_csv(BASE_DIR / "store.csv")
 
     store["CompetitionDistance"]       = store["CompetitionDistance"].fillna(store["CompetitionDistance"].median())
     store["CompetitionOpenSinceMonth"] = store["CompetitionOpenSinceMonth"].fillna(0)
@@ -62,15 +65,15 @@ def load_data():
 
 @st.cache_data
 def load_predictions():
-    return pd.read_csv("outputs\\final_predictions.csv")
+    return  pd.read_csv(BASE_DIR / "final_predictions.csv")
 
 # Load model once — @st.cache_resource keeps it in memory
 @st.cache_resource
 def load_model_and_scalers():
     from tensorflow.keras.models import load_model
-    model          = load_model("dashboard/best_model.keras")
-    feature_scaler = joblib.load("dashboard/feature_scaler.pkl")
-    target_scaler  = joblib.load("dashboard/target_scaler.pkl")
+    model = load_model(BASE_DIR / "best_model.keras")
+    feature_scaler = joblib.load(BASE_DIR / "feature_scaler.pkl")
+    target_scaler = joblib.load(BASE_DIR / "target_scaler.pkl")
     return model, feature_scaler, target_scaler
 
 df     = load_data()
